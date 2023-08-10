fb1ca828398f17d65f3a9a6f3d7569515ebd240b
Edgar Bodiaj <edgar.bodiaj@nottingham.ac.uk>
2023-08-10 12:09:53 +0100

    Unconstrained roation on toplight, added toggle and reset

--- Assets/Scripts/UIControl.cs
+++ Assets/Scripts/UIControl.cs
@@ -9,12 +9,42 @@ public class UIControl : MonoBehaviour
 
     public GameObject Tool_Torch;
     public GameObject Tool_TopLight;
-    GameObject Tool_ToplightRotationSphere;
+    public GameObject Tool_ToplightRotationSphere;
 
     public GameObject Tool_TopLightRotationControl;
+
+    public List<string> Tool_RotateDir;
+    public Quaternion Tool_RotationQuart;
+    public Vector3 Tool_EulerRot;
+
     private void Start()
     {
-        Tool_ToplightRotationSphere = Tool_TopLight.GetComponentInParent<Transform>().gameObject;
+        Tool_ToplightRotationSphere = Tool_TopLight.GetComponentInParent<Transform>().parent.gameObject;
+        Tool_RotateDir = new List<string>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Tool_RotateDir.Count > 0)
+        {
+            Tool_EulerRot = new Vector3(0, 0, 0);
+            //Positive X
+            if (Tool_RotateDir.Exists(x => x == "00")) Tool_EulerRot.x = 2;
+            //Negative X
+            else if (Tool_RotateDir.Exists(x => x == "11")) Tool_EulerRot.x = -2;
+            //Negative Z
+            if (Tool_RotateDir.Exists(x => x == "01")) Tool_EulerRot.z = -2;
+            //Positive Z
+            else if (Tool_RotateDir.Exists(x => x == "10")) Tool_EulerRot.z = 2;
+            Debug.Log(Tool_EulerRot);
+            Tool_EulerRot.y = 0;
+            Tool_ToplightRotationSphere.transform.Rotate(Tool_EulerRot);
+        }
+    }
+
+    private void Update()
+    {
+        
     }
 
     public void Toolbar_ToggleToolbox()
@@ -28,6 +58,11 @@ public class UIControl : MonoBehaviour
         Tool_Torch.SetActive(!Tool_Torch.activeInHierarchy);
     }
 
+    public void Toolbar_Toplight_ToggleToolbar()
+    {
+        Tool_TopLightRotationControl.SetActive(!Tool_TopLightRotationControl.activeInHierarchy);
+    }
+
     public void Toolbar_Toplight_Rotate(string dir)
     {
         //Direction Code:
@@ -35,24 +70,19 @@ public class UIControl : MonoBehaviour
         //01 = Right, Z-
         //11 = Down, X-
         //10 = Left, Z+
-        switch (dir)
-        {
-            case "00":
-                Debug.Log("Up");
-                break;
-            case "01":
-                Debug.Log("Right");
-                break;
-            case "11":
-                Debug.Log("Down");
-                break;
-            case "10":
-                Debug.Log("Left");
-                break;
-            default:
-                break;
-        }
+        if(Tool_RotateDir.Exists(x => x == dir)) Tool_RotateDir.Remove(dir);
+        else Tool_RotateDir.Add(dir);
 
+    }
+
+    public void Toolbar_Toplight_Reset()
+    {
+        Tool_ToplightRotationSphere.transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+
+    public void Toolbar_Toplight_Toggle()
+    {
+        Tool_TopLight.SetActive(!Tool_TopLight.activeInHierarchy);
     }
 
     public void Toolbar_ToggleLightAngleControl()
