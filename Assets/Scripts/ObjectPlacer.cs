003b37a74a302f47832c3bc3252c0dd2acfc90ca
Edgar Bodiaj <edgar.bodiaj@nottingham.ac.uk>
2023-09-27 12:02:04 +0100

    More scale changes

--- Assets/Scripts/ObjectPlacer.cs
+++ Assets/Scripts/ObjectPlacer.cs
@@ -83,6 +83,7 @@ ARAnchor CreateAnchor(in ARRaycastHit hit) {
             clo.transform.localEulerAngles = new Vector3(0,180,0);
             //clo.transform.localEulerAngles = Vector3.zero;
             clo.transform.localPosition = Vector3.zero;
+            clo.transform.localScale = new Vector3(100,100,100);
             go.GetComponent<Renderer>().enabled = false;
 
             Control.Control_SetActiveObject(clo);
